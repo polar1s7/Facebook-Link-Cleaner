985435b951d4ae58b1aafa9d9cd3d420349aeb9e
Joshua Cruz <60358264+polar1s7@users.noreply.github.com>
2021-05-18 04:58:46 +0800

    Add files via upload

---
+++ content.js
@@ -0,0 +1,16 @@
+function deleteAllHref(){
+    var getHref = document.getElementsByTagName("a");
+    var arrLen = getHref.length;
+    for(var i =0; i < arrLen; i++){
+        var hrefLoop = getHref[i];
+        var getLoopAttribute = hrefLoop.getAttribute("href")
+        hrefLoop.setAttribute("href", splitTheShitOut(getLoopAttribute));
+    }
+}
+function splitTheShitOut(link){
+	return link.split('__cft__')[0];
+}
+function startTheUseless(){
+    setInterval(deleteAllHref, 0); // much faster if 0 
+}
+$(document).ready(startTheUseless);
